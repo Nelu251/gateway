7da0d9f51019f2b371dc21c1f031adb40dcb7be1
isturcanu <91874438+isturcanu@users.noreply.github.com>
2022-10-13 01:59:42 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using WebApp1.Models;
+
+namespace WebApp1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("getAllMovies")]
+        public IActionResult Get(long id)
+        {
+            var client = new RestClient();
+            var request = new RestRequest($"http://localhost:8080/home/movie/{id}", Method.Get);
+            var response = client.Execute(request);
+            return Ok(response.Content);
+        }
+
+        [HttpPost("postMovie")]
+        public IActionResult Post(Movie movie)
+        {   
+            var client = new RestClient();
+            var request = new RestRequest($"http://localhost:8080/home/addMovie/{movie.Id}", Method.Post);
+            request.AddJsonBody(movie);
+            var response = client.Execute(request);
+            return Ok(response.StatusCode);
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApp1.Models
+{
+    public class Movie
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public long Year { get; set; }
+        public string Director { get; set; }
+        public int Stars { get; set; }
+    }
+}
